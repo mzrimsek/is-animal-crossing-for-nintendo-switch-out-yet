072549b61dbc59b67a38b65f082fef85e348b384
Mike Zrimsek <mikezrimsek@gmail.com>
2018-01-05 10:42:18 -0500

    update to tracking and styling

--- public/scripts.js
+++ public/scripts.js
@@ -1,6 +1,7 @@
-const trackClick = (site, community) => {
+function trackClick(site, community) {
     gtag('event', 'click', {
         'event_category': site,
         'event_label': community
     });
+    console.log(site, community);
 };
