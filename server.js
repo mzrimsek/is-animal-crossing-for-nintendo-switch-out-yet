7c03590b62350dfdc2f901b85e924ff3f72ecae1
Mike Zrimsek <mikezrimsek@gmail.com>
2018-01-14 13:59:26 -0500

    fix port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
-const port = 3002;
+const port = 3000;
 
 const app = express();
 app.use(bodyParser.json());
